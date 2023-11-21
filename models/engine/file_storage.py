e1373b2c9b33a420916786ff3e7129c0dc1618f1
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2023-11-21 17:47:09 +0300

    new public instace method to delte objects, updating all() to return objects of a class

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,15 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is not None:
+            cls_objects = {}
+            for k, v in self.__objects.items():
+                cls_objects.update({k: V})
+            return cls_objects
+        else:
+            return FileStorage.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -48,3 +54,8 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+    
+    def delete(self, obj=None):
+        """Deletes the obj if in __objects"""
+        if obj is not None:
+            del self.__objects["{}.{}".format(type(obj).__name__, obj.id)]
