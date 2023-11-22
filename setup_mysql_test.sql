369f8086725f2afceb897bf7dce5037bcb6115fc
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2023-11-22 18:20:10 +0300

    correct the syntax of .mysql files

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,4 +1,4 @@
---Script that prepares a MySQL server for the project
+-- Script that prepares a MySQL server for the project
 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER
@@ -6,10 +6,8 @@ CREATE USER
     IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES
     ON `hbnb_test_db`.*
-    TO 'hbnb_test'@'localhost'
-    IDENTIFIED BY 'hbnb_test_pwd';
+    TO 'hbnb_test'@'localhost';
 GRANT SELECT
     ON `performance_schema`.*
-    TO 'hbnb_test'@'localhost'
-    IDENTIFIED BY 'hbnb_test_pwd';
+    TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
