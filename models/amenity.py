1dddeb90dd89eaae1412727a508fba8f5c759d0d
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2023-11-23 14:52:40 +0300

    task 10

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from os import getenv
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 
 
 class Amenity(BaseModel):
-    name = ""
+    """The amenity class."""
+    __tablename__ = "amenties"
+    
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
+    
+    else:
+        name = ""
