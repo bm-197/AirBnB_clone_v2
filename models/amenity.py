3685abffd6d65f5a026fab3ebab701ebddcbb8c5
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2023-11-23 14:54:05 +0300

    task 10

--- models/amenity.py
+++ models/amenity.py
@@ -3,7 +3,7 @@
 from os import getenv
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-
+from sqlalchemy.orm import relationship
 
 class Amenity(BaseModel):
     """The amenity class."""
@@ -11,6 +11,6 @@ class Amenity(BaseModel):
     
     if getenv("HBNB_TYPE_STORAGE") == "db":
         name = Column(String(128), nullable=False)
-    
+        place_amenities = relationship("Place", secondary="place_amenity", viewonly=False)
     else:
         name = ""
