873191722a8b70f3a390457192969bbaf0eb5de5
ZorEl212 <yabsirad212@gmail.com>
2023-12-08 03:26:39 -0500

    Revert "task 7"
    This reverts commit 557b9785840d8ed6953a9ee02f4cd9ff8de04147.

--- models/review.py
+++ models/review.py
@@ -1,20 +1,10 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from os import getenv
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from models.base_model import BaseModel
 
 
 class Review(BaseModel):
     """ Review classto store review information """
-
-    __tablename__ = "reviews"
-
-    if getenv("HBVB_TYPE_STORAGE") == "db":
-        place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
-        user_id = Column(String(60), ForeignKey("user.id"), nullable=False)
-        text = Column(String(1024), nullable=False)
-    else:
-        place_id = ""
-        user_id = ""
-        text = ""
+    place_id = ""
+    user_id = ""
+    text = ""
--- models/user.py
+++ models/user.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 """This module defines a class User"""
+from os import getenv
+
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 from models.base_model import BaseModel
 
 
