da017de53e190b40a51f332aef3b72ed2b5375b6
ZorEl212 <yabsirad212@gmail.com>
2023-12-08 03:27:28 -0500

    Revert "task 10"
    This reverts commit 1dddeb90dd89eaae1412727a508fba8f5c759d0d.
    
    Signed-off-by: ZorEl212 <yabsirad212@gmail.com>

--- models/amenity.py
+++ models/amenity.py
@@ -1,16 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from os import getenv
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+from models.base_model import BaseModel
+
 
 class Amenity(BaseModel):
-    """The amenity class."""
-    __tablename__ = "amenties"
-    
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        name = Column(String(128), nullable=False)
-        place_amenities = relationship("Place", secondary="place_amenity", viewonly=False)
-    else:
-        name = ""
+    name = ""
