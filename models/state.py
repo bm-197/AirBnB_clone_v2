40442c8300d3b90fdb9a087a69ff3a2b9f66684a
ZorEl212 <yabsirad212@gmail.com>
2023-12-08 03:43:24 -0500

    Revert to state before merge
    Signed-off-by: ZorEl212 <yabsirad212@gmail.com>

--- models/state.py
+++ models/state.py
@@ -1,21 +1,27 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+from os import getenv
 from models.base_model import Base, BaseModel
 from sqlalchemy import Column, ForeignKey, String
+from models.city import City
 from sqlalchemy.orm import relationship
-
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City",  backref="state", cascade="delete")
+    if getenv('HBNB_TYPE_STORAGE') == "db":
+        from models.city import City
+        name = Column(String(128), nullable=False)
+        cities = relationship("City",  backref="state", cascade="delete")
 
-    @property
-    def cities(self):
-        """Get a list of all related City objects."""
-        city_list = []
-        for city in list(storage.all(City).values()):
-            if city.state_id == self.id:
-                city_list.append(city)
-        return city_list
+        @property
+        def cities(self):
+            from models import storage
+            """Get a list of all related City objects."""
+            city_list = []
+            for city in list(storage.all(City).values()):
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
+    else:
+           name = ""
