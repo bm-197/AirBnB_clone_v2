eab1b7afb92a5836f659336dd3b333f658b17458
ZorEl212 <yabsirad212@proton.me>
2024-01-30 17:19:12 +0300

    Fix pep-8 style and add doc
    Signed-off-by: ZorEl212 <yabsirad212@proton.me>

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
+
+"""Create hello route for the flask app"""
+
+
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello():
     return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
     app.run()
