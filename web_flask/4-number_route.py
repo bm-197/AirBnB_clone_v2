8f81df43d94f214e17e8c17fb9615e3458ecd698
ZorEl212 <yabsirad212@proton.me>
2024-01-31 18:05:24 +0300

    Add number route to the app
    Signed-off-by: ZorEl212 <yabsirad212@proton.me>

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+"""Create hello route for the flask app"""
+
+
+from flask import Flask, abort
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def cisfun(text):
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route("/python/<text>", strict_slashes=False)
+@app.route("/python", strict_slashes=False)
+def python(text="is cool"):
+    return f"Python {text.replace('_', ' ')}"
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def numbers(n):
+    return f"{n} is a number" if isinstance(n, int) else abort(404)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
