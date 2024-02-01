f3914a1248235a7589b75746d82f8a83bb5a1ba4
ZorEl212 <yabsirad212@proton.me>
2024-02-01 13:16:51 +0300

    Refactor states_list route and add teardown_db function
    Signed-off-by: ZorEl212 <yabsirad212@proton.me>

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,25 +3,23 @@
 starts a Flask web application
 """
 
-
 from flask import Flask, render_template
+from models import *
 from models import storage
-from models.state import State
-
 app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """display a HTML page with the states listed in alphabetical order"""
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
+
 
 @app.teardown_appcontext
 def teardown_db(exception):
-    """Close database connection after the request"""
+    """closes the storage on teardown"""
     storage.close()
 
-
-@app.route("/states_list", strict_slashes=False)
-def states_list():
-    all_states = storage.all(State)
-    all_states = dict(sorted(all_states.items(), key=lambda x: x[1]['name']))
-    return render_template('7-states_list.html', states_list=all_states)
-
-
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port="5000")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
