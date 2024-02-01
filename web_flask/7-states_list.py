d0dfa6bcb26c596b52a8ceded7554ae5b047bd78
ZorEl212 <yabsirad212@proton.me>
2024-02-01 13:14:35 +0300

    loosing my mind
    Signed-off-by: ZorEl212 <yabsirad212@proton.me>

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-
-"""Flask Script to fetch all states from database"""
+"""
+starts a Flask web application
+"""
 
 
 from flask import Flask, render_template
@@ -23,4 +24,4 @@ def states_list():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port="5000")
