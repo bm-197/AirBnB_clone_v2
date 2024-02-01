8393ab06949583294cda008839b5f38c5a5676ec
ZorEl212 <yabsirad212@proton.me>
2024-02-01 10:47:00 +0300

    Refactor DBStorage to comply with pep-8
    Signed-off-by: ZorEl212 <yabsirad212@proton.me>

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -14,6 +14,7 @@ from models.review import Review
 from models.state import State
 from models.user import User
 
+
 class DBStorage:
     """This class manages storage of hbnb models in structured format"""
 
@@ -22,11 +23,16 @@ class DBStorage:
 
     def __init__(self):
         """creates the engine"""
-        self.__engine = create_engine("mysql+mysqldb://{}:{}@{}/{}".format(getenv("HBNB_MYSQL_USER"), getenv("HBNB_MYSQL_PWD"), getenv("HBNB_MYSQL_HOST"), getenv("HBNB_MYSQL_DB")), pool_pre_ping=True)
-        
+        self.__engine = create_engine(
+            "mysql+mysqldb://{}:{}@{}/{}".format(getenv("HBNB_MYSQL_USER"),
+                                                 getenv("HBNB_MYSQL_PWD"),
+                                                 getenv("HBNB_MYSQL_HOST"),
+                                                 getenv("HBNB_MYSQL_DB")),
+            pool_pre_ping=True)
+
         if getenv("HBNB_ENV") == "test":
             Base.metadata.drop_all(self.__engine)
-    
+
     def all(self, cls=None):
         """Query the table on the current database session"""
         objs = []
@@ -41,12 +47,13 @@ class DBStorage:
         # Exclude '_sa_instance_state' from the dictionaries
         result = {
             "{}.{}".format(type(o).__name__, o.id): {
-                key: value for key, value in o.__dict__.items() if key != '_sa_instance_state'
+                key: value for key, value in o.__dict__.items()
+                       if key != '_sa_instance_state'  # noqa: E131
             } for o in objs
         }
 
         return result
-    
+
     def new(self, obj):
         '''adds the obj to the current db session'''
         if obj is not None:
@@ -57,27 +64,23 @@ class DBStorage:
             except Exception as ex:
                 self.__session.rollback()
                 raise ex
-    
+
     def save(self, obj=None):
         """Commit all changes of the current database session."""
         self.__session.commit()
-    
+
     def delete(self, obj=None):
         """Delete obj from the current database session."""
         if obj is not None:
             self.__session.delete(obj)
-    
+
     def reload(self):
         """Create all tables in the database and initialize a new session."""
         Base.metadata.create_all(self.__engine)
-        session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        session_factory = sessionmaker(
+            bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(session_factory)
         self.__session = Session()
 
     def close(self, obj=None):
         self.__session.close()
-
-
-        
-
-    
